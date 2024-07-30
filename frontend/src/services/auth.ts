420f4d83cbac7d7f497b730725327a21f0a51af4
mentorvikash <mentorvikash@gmail.com>
2024-07-30 11:26:32 +0530

    setup intial frontend project

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000/api/auth";
+
+interface IUser {
+  username?: string;
+  email: string;
+  password: string;
+}
+
+export const resister = async ({ email, password, username }: IUser) => {
+  return await axios.post(`${baseUrl}/signup`, {
+    username,
+    email,
+    password,
+  });
+};
+
+export const login = async ({ email, password }: IUser) => {
+  return await axios.post(`${baseUrl}/login`, {
+    email,
+    password,
+  });
+};
+
+export const logout = async () => {
+  return localStorage.removeItem("user");
+};
+
+export const getCurrentUser = async () => {
+  const user = localStorage.getItem("user");
+  if (typeof user == "string") {
+    return JSON.parse(user);
+  }
+};
